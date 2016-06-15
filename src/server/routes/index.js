b28817c3632dc4d36290acbc048d13c39e9f7d5b
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-15 09:47:51 -0600

    Inital scour of href's in a tags

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,4 +7,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/scour', function(req, res, next) {
+  // The URL we will scour
+  url = req.query.url || 'http://www.sweetactionicecream.com/';
+
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
+
+  request(url, function(error, response, html) {
+
+      // First we'll check to make sure no errors occurred when making the request
+      if (error) { console.log(error); }
+
+      else {
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+        var parsedHTML = cheerio.load(html);
+
+        parsedHTML('a').map(function(i, link) {
+          var href = cheerio(link).attr('href');
+          console.log(href);
+        });
+      }
+  });
+
+});
+
 module.exports = router;
